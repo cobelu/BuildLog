7eee1373aff4bf3e9e76bd52bf09c41d482ed924
cobelu <imamac96@gmail.com>
2019-08-22 09:12:59 -0400

    Menu Bar work

--- src/com/cobelu/build_log/view/ApplicationFrame.java
+++ src/com/cobelu/build_log/view/ApplicationFrame.java
@@ -1,10 +1,15 @@
 package com.cobelu.build_log.view;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import com.cobelu.build_log.model.Model;
 
@@ -28,19 +33,35 @@ public class ApplicationFrame extends JFrame {
 		JMenuItem mntmNewEntry = new JMenuItem("New Entry");
 		mnFile.add(mntmNewEntry);
 		
+		mnFile.addSeparator();
+		
 		JMenuItem mntmNewProject = new JMenuItem("New Project");
 		mnFile.add(mntmNewProject);
 		
 		JMenuItem mntmOpenProject = new JMenuItem("Open Project");
 		mnFile.add(mntmOpenProject);
 		
+		mnFile.addSeparator();
+		
 		JMenuItem mntmExit = new JMenuItem("Exit");
+		mntmExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		mnFile.add(mntmExit);
 		
 		JMenu mnHelp = new JMenu("Help");
 		menuBar.add(mnHelp);
 		
 		JMenuItem mntmAbout = new JMenuItem("About");
+		mntmAbout.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Build Log");
+			}
+		});
 		mnHelp.add(mntmAbout);
 		
 		entryPanel = new EntryPanel(model.getEntryModel());
